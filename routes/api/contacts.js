efc91c7da250c24b648219e89b8586e235836024
Aleksandr Harianin <onlydipro@gmail.com>
2023-12-01 11:42:07 +0200

    done

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,21 +1,14 @@
 const express = require('express');
 const router = express.Router();
 const ctrl = require('../../controllers/contacts');
-const { ctrlWrapper } = require('../../utils');
 const { validateBody } = require('../../middlewares');
 const schemas = require('../../schemas/contacts');
-router.get('/', ctrlWrapper(ctrl.listContacts));
-router.get('/:contactId', ctrlWrapper(ctrl.getContactById));
-router.post('/', validateBody(schemas.addSchema), ctrlWrapper(ctrl.addContact));
 
-router.put('/:contactId', validateBody(schemas.addSchema), ctrlWrapper(ctrl.updateContactById));
-
-router.patch(
-  '/:contactId/favorite',
-  validateBody(schemas.patchSchema),
-  ctrlWrapper(ctrl.updateStatusContact)
-);
-
-router.delete('/:contactId', ctrlWrapper(ctrl.removeContact));
+router.get('/', ctrl.listContacts);
+router.get('/:contactId', ctrl.getContactById);
+router.post('/', validateBody(schemas.addSchema), ctrl.addContact);
+router.put('/:contactId', validateBody(schemas.addSchema), ctrl.updateContactById);
+router.patch('/:contactId/favorite', validateBody(schemas.patchSchema), ctrl.updateStatusContact);
+router.delete('/:contactId', ctrl.removeContact);
 
 module.exports = router;
